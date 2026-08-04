fd69cdd95014b627334d2a54a1119d24f15b8bf8
yudongrui <yudongrui@huawei.com>
2026-08-04 11:36:21 +0800

    add timeout config

--- ais_bench/benchmark/global_consts.py
+++ ais_bench/benchmark/global_consts.py
@@ -1,6 +1,10 @@
+import os
+
+
 WORKERS_NUM = 0
 MAX_CHUNK_SIZE = 2**16
-REQUEST_TIME_OUT = None
+_request_timeout = os.getenv("AIS_BENCH_REQUEST_TIMEOUT")
+REQUEST_TIME_OUT = float(_request_timeout) if _request_timeout else None
 
 from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
 LOG_LEVEL = INFO # log level, choose from DEBUG, INFO, WARNING, ERROR, CRITICAL
